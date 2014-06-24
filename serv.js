2046481b8ab9bdd2f494e901ca21e66cc105c753
HE Shi-Jun <hax@heshijun.net>
2014-06-24 15:36:52 +0800

    initial import

---
+++ serv.js
@@ -0,0 +1,17 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+//require('demoshare')(http)
+
+app.get('/', function (req, res) {
+	res.sendfile('index.html')
+})
+
+io.on('connection', function (socket) {
+	socket.emit('chat', 'Welcome to chat demo!')
+	socket.on('chat', function (text) {
+		io.emit('chat', text)
+	})
+})
+
+http.listen(80)
